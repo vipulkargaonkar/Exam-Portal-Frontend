83757b9223ec8f098da144a72022a90d28346345
vipul.kargaonkar <vipulkargaonkar@gmail.com>
2025-05-04 00:22:40 +0530

    Added login logic

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,17 +4,66 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { UserServiceService } from '../../services/user-service.service';
 import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 import { MatCardModule } from '@Angular/material/card';
 import swal from 'sweetalert2';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
   imports: [MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule, HttpClientModule, MatSnackBarModule, MatCardModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  standalone: true,
+  providers: [
+      LoginService
+    ]
 })
 export class LoginComponent {
 
+  constructor(private snack : MatSnackBar, private login : LoginService) {
+
+  }
+
+  loginData = {
+    username : '',
+    password : ''
+  }
+
+  formSubmit() {
+    if(this.loginData.username.trim() === '' || this.loginData.username === null) {
+        this.snack.open("User name is required!!!" , '', {
+          duration: 3000
+        })
+        return;
+    }
+
+    if(this.loginData.password.trim() === '' || this.loginData.password === null) {
+        this.snack.open("Password is required!!!" , '', {
+          duration: 3000
+        })
+        return;
+      }
+
+      this.login.generateToken(this.loginData).subscribe(
+        (data:any) => {
+           console.log(data);
+                  swal.fire({
+                    title: "Success",
+                    text: "Login successful " + data.username,
+                    icon: "success"
+                  });
+        },
+        (error) => {
+          console.log(error);
+                  swal.fire({
+                    title: "Error",
+                    text: "Something went wrong!!",
+                    icon: "error"
+                  });
+        }
+      )
+  }
+
+  
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http : HttpClient) { }
+
+  public generateToken(loginData : any) {
+    return this.http.post(`${baseUrl}/generate-token`, loginData);
+  }
+
+  public loginUser(token: string) {
+    localStorage.setItem('token', token);
+    return true;
+  }
+
+  public isLoggedIn() {
+    let tokenStr=localStorage.getItem("token");
+    if(tokenStr==undefined || tokenStr=='' || tokenStr==null) {
+      return false
+    } else {
+      return true;
+    }
+  }
+
+  public logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    return true;
+  }
+
+
+  public getToken() {
+    return localStorage.getItem('token');
+  }
+
+  public setUser(user: any) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  public getUser() {
+    let userStr = localStorage.getItem("user");
+    if(userStr!=null) {
+      return JSON.parse(userStr);
+    }
+    else {
+      this.logout();
+      return null;
+    }
+  }
+
+  public getUserRole() {
+    let user = this.getUser();
+    return user.authorities[0].authority;
+  }
+}
