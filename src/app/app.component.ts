2d3cccd95275b382005529715b4b5b65bc23a19a
vipul.kargaonkar <vipulkargaonkar@gmail.com>
2025-06-09 00:45:34 +0530

    Added loader to the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxUiLoaderModule  } from "ngx-ui-loader";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, NavbarComponent, MatInputModule, MatFormFieldModule, HttpClientModule],
+  imports: [NgxUiLoaderModule, RouterOutlet, MatButtonModule, NavbarComponent, MatInputModule, MatFormFieldModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptorProviders } from './services/auth.interceptor';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { NgxUiLoaderModule, NgxUiLoaderHttpModule } from 'ngx-ui-loader';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), authInterceptorProviders, provideHttpClient(withInterceptorsFromDi())
+  providers: [importProvidersFrom(
+      NgxUiLoaderModule,
+      NgxUiLoaderHttpModule.forRoot({ showForeground: true })
+    ), 
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), authInterceptorProviders, provideHttpClient(withInterceptorsFromDi())
   ]
 };
